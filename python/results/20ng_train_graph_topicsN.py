f57736af0728a9073f0cbc5a46d2c460fc531f4b
marshsh <mar.freig@gmail.com>
2019-05-22 01:52:49 -0500

    .

--- python/results/20ng_train_graph_topicsN.py
+++ python/results/20ng_train_graph_topicsN.py
@@ -6,17 +6,17 @@ x_values = v.x_values
 
 
 for topicN in x_values:
-	args = arguments.preMain(["-e", "smh", "-tN", "topicN", "-tS", "2", "-coo", "0.02"])
+	args = arguments.preMain(["-e", "smh", "-tN", topicN, "-tS", "2", "-coo", "0.02"])
 	train.main(args)
 
 for topicN in x_values:
-	args = arguments.preMain(["-e", "smh", "-tN", "topicN", "-tS", "2", "-coo", "0.03"])
+	args = arguments.preMain(["-e", "smh", "-tN", topicN, "-tS", "2", "-coo", "0.03"])
 	train.main(args)
 
 for topicN in x_values:
-	args = arguments.preMain(["-e", "smh", "-tN", "topicN", "-tS", "3", "-coo", "0.02"])
+	args = arguments.preMain(["-e", "smh", "-tN", topicN, "-tS", "3", "-coo", "0.02"])
 	train.main(args)
 
 for topicN in x_values:
-	args = arguments.preMain(["-e", "smh", "-tN", "topicN", "-tS", "3", "-coo", "0.03"])
+	args = arguments.preMain(["-e", "smh", "-tN", topicN, "-tS", "3", "-coo", "0.03"])
 	train.main(args)
