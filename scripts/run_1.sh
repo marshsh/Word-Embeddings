79bf700235bcf38e09ddc764e47997ccf9557506
marshsh <mar.freig@gmail.com>
2019-06-03 18:30:23 -0500

    .

--- scripts/run_1.sh
+++ scripts/run_1.sh
@@ -10,10 +10,19 @@ coos='0.04 0.06 0.08 0.10'
 for coo in $coos
 do
 	python ./python/train.py -e smh -tN 7000 -tS 2 -coo $coo --overlap 0.8
-	echo ********************************************
+	echo '********************************************'
 	echo Next experiments
-	echo ********************************************
+	echo '********************************************'
 done
+
+for coo in $coos
+do
+	python ./python/train.py -e smh -tN 7000 -tS 3 -coo $coo --overlap 0.8
+	echo '********************************************'
+	echo Next experiments
+	echo '********************************************'
+done
+
 
 echo All done
 
