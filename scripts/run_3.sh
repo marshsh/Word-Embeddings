e3a6029bb416241ba10fea423cd0995c99a774be
marshsh <mar.freig@gmail.com>
2019-06-09 13:28:51 -0500

    .

---
+++ scripts/run_3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Script to run experiments.
+#
+
+
+print_space () {
+	echo
+	echo
+	echo
+	echo
+	echo '********************************************'
+	echo Next experiments
+	echo '********************************************'
+	echo
+	echo
+	echo
+	echo
+
+}
+
+
+print_space
+
+python ./python/train.py -e smh -tN 7000 -tS 3 -coo 0.35 --overlap 0.8 --nameBoard 'yaya'
+
+print_space
+
+python ./python/train.py -e smh -tN 7000 -tS 3 -coo 0.45 --overlap 0.8 --nameBoard 'yaya'
+
+print_space
+
+
+coos='0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6'
+
+for coo in $coos
+do
+	python ./python/train.py -e smh -tN 7000 -tS 4 -coo $coo --overlap 0.8 --nameBoard 'yaya'
+
+done
+
+
+echo
+echo
+echo All done
+
+
+
+
+
