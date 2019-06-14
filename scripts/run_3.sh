c122add96b6154ca552234b1f3260a47cd2d0491
marshsh <mar.freig@gmail.com>
2019-06-14 03:20:40 -0500

    .

--- scripts/run_3.sh
+++ scripts/run_3.sh
@@ -24,15 +24,28 @@ print_space
 
 
 # coos='0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6'
-coos='0.4 0.5 0.6'
+# coos='0.4 0.5 0.6'
 
-for coo in $coos
-do
-	python ./python/train.py -e smh -tN 7000 -tS 4 -coo $coo --overlap 0.8 --nameBoard 'yaya'
+# for coo in $coos
+# do
+# 	python ./python/train.py -e smh -tN 7000 -tS 4 -coo $coo --overlap 0.8 --nameBoard 'yaya'
 
-	print_space
+# 	print_space
 
-done
+# done
+
+python ./python/train.py -e smh -tN 7000 -tS 2 -coo 0.3 --overlap 0.8 --nameBoard 'ya_StopWords'
+print_space
+python ./python/train.py -e smh -tN 7000 -tS 3 -coo 0.35 --overlap 0.8 --nameBoard 'ya_StopWords'
+print_space
+python ./python/train.py -e smh -tN 7000 -tS 4 -coo 0.45 --overlap 0.8 --nameBoard 'ya_StopWords'
+print_space
+python ./python/train.py -e smh -tN 7000 -tS 5 -coo 0.45 --overlap 0.8 --nameBoard 'ya_StopWords'
+print_space
+python ./python/train.py -e smh -tN 7000 -tS 5 -coo 0.5 --overlap 0.8 --nameBoard 'ya_StopWords'
+print_space
+python ./python/train.py -e smh -tN 7000 -tS 5 -coo 0.55 --overlap 0.8 --nameBoard 'ya_StopWords'
+print_space
 
 
 echo
